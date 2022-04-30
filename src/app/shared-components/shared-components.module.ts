31617d4fb69d8ab2eb48da272dba1284c6f0b503
Dennis Lange <dennis.lange95@web.de>
2022-04-30 12:33:22 +0200

    structure app. generate needed components

--- src/app/shared-components/shared-components.module.ts
+++ src/app/shared-components/shared-components.module.ts
@@ -2,16 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ServicesComponent } from './services/services.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    AboutmeComponent
-  ],
+    AboutmeComponent,
+    ServicesComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    NavigationComponent],
   exports: [
     HeaderComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    ServicesComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule
