0b487e8be302c34c4de87fc6af3be8aa0f69fdde
snackaholic <36051495+snackaholic@users.noreply.github.com>
2022-05-01 18:18:03 +0200

    experience & header

---
+++ src/app/core/types/experience.ts
@@ -0,0 +1,6 @@
+export interface IExperience {
+    period : string,
+    company : string,
+    title: string,
+    points : string[]
+}
--- src/app/shared-components/experience/experience.component.ts
+++ src/app/shared-components/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IExperience } from 'src/app/core/types/experience';
 
 @Component({
   selector: 'app-experience',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+
+  experiences : IExperience[] = [
+    {
+      company : "Jobware GmbH",
+      period : "Oktober 2020 - laufend",
+      title : "Fullstack Softwareentwicklung",
+      points : ["Relaunch des Webauftritts der Jobware GmbH", "Konzeptionierung und Entwicklung diverser Microservices für die Webseite"]
+    },
+    {
+      company : "Jobware GmbH",
+      period : "Mai 2018 - Oktober 2020",
+      title : "Backend Softwareentwicklung",
+      points : ["Implementierung, Konfiguration und Wartung von Schnittstellenlösungen zu Bewerber-Management-Systemen und Karriere-Portalen", "Portierung von Altsystemen in neue Technologien"]
+    },
+    {
+      company : "Jobware GmbH",
+      period : "Juni 2017 - Mai 2018",
+      title : "Frontend Softwareentwicklung",
+      points : ["Entwurf und Umsetzung des Projekts https://www.jobs-für-meine-region.de", "Realisierung und Betreuung der regionalen Portale unter JFMS"]
+    }
+  ];
 
   constructor() { }
 
