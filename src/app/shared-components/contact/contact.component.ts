359053ac36bcc934603582edc8e9a332305678b7
snackaholic <36051495+snackaholic@users.noreply.github.com>
2022-05-01 19:11:36 +0200

    contact

--- src/app/shared-components/contact/contact.component.ts
+++ src/app/shared-components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IService } from 'src/app/core/types/service';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  location : IService = {
+    heading : "Standort",
+    text : "Dennis Lange\nDahler Heide 69\n33100 Paderborn"
+  };
+
+  email : IService = {
+    heading : "E-Mail",
+    text : "mailto.dennis.lange@gmail.com"
+  };
+
+  phone : IService = {
+    heading : "Telefon",
+    text : "016093507143"
+  };
 
   constructor() { }
 
