b337f024a2db93aaf8d84e7507b938af4ad10366
Dennis Lange <dennis.lange95@web.de>
2023-12-18 15:27:36 +0100

    calculate age

--- src/app/shared-components/aboutme/aboutme.component.ts
+++ src/app/shared-components/aboutme/aboutme.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutme.component.scss']
 })
 export class AboutmeComponent implements OnInit {
+  yearsOnEarth!: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.yearsOnEarth = this.calculateCurrentAge(new Date('1995-07-16T01:12:00'));
+  }
+
+  calculateCurrentAge(birthday : Date) {
+    var millisecondsPassedSinceBirthday = Date.now() - birthday.getTime();
+    return Math.abs(new Date(millisecondsPassedSinceBirthday).getUTCFullYear() - 1970);
   }
 
 }
